f75d5f091e183039dce77745011c1a531ae39f16
Paul Staples <pmkstaples@gmail.com>
2020-09-10 12:14:45 -0600

    Add files via upload

---
+++ build/precache-manifest.58889f74bfbac49a00d4b989a58f04ae.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd084984a853966657116b1b9ac95e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b03a77bfd18dc69f166d",
+    "url": "/static/css/main.10c53eaa.chunk.css"
+  },
+  {
+    "revision": "a270ede8733c5dab7cee",
+    "url": "/static/js/2.0d32c39e.chunk.js"
+  },
+  {
+    "revision": "c21a78784305bd0b125a86fae7d9b7e4",
+    "url": "/static/js/2.0d32c39e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b03a77bfd18dc69f166d",
+    "url": "/static/js/main.3e4a7f70.chunk.js"
+  },
+  {
+    "revision": "184f40e739f4d5ba6097",
+    "url": "/static/js/runtime-main.7c3402f0.js"
+  },
+  {
+    "revision": "90d940c48958578a3aae21f192902484",
+    "url": "/static/media/croppedface.90d940c4.jpg"
+  },
+  {
+    "revision": "44e9dd7f6287032729f17690eb7fd172",
+    "url": "/static/media/sibg.44e9dd7f.png"
+  },
+  {
+    "revision": "3d7eef17e8080b682a41d6f6372c7ae3",
+    "url": "/static/media/snakescreen.3d7eef17.PNG"
+  },
+  {
+    "revision": "1e715af8567f2554722a16aa3b87a1b0",
+    "url": "/static/media/tanksscreen.1e715af8.png"
+  }
+]);
